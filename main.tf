e9dee76e9e5531bc046f6bd79c47ccf3c2a65ed7
nivav1 <139040322+nivav1@users.noreply.github.com>
2024-10-21 16:12:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,13 @@ provider "aws" {
   region = "il-central-1"
 }
 
-tags = {
-  name = "myweb"
-}
 
 resource "aws_instance" "my_server" {
   ami = "ami-0399b1f276c3e2999"
   instance_type = "t3.micro"
+}
+
+
+tags = {
+  name = "myweb"
 }
